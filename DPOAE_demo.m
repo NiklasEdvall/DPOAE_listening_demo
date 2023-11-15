d3d35ea5595dd9369d5bfcf02306a7833a7e8419
Niklas Edvall <niklas.edvall@ki.se>
2023-11-15 16:48:37 +0100

    added plot

--- DPOAE_demo.m
+++ DPOAE_demo.m
@@ -18,11 +18,33 @@ t3  = linspace(0, 7, 7*fs);
 w2 = 2*pi*700;
 F1 = sin(w2*t3);
 
+% Lower amplitude
 F1 = F1 * 0.5;
-F2 = [s s2] * 0.5;
+F2 = [s s2] * 0.5; %And append tone+chirp
 
+% Combine and adjust amplitude
 F3 = (F1+F2)*0.6;
 
-audiowrite("F1_sound.wav", F1, fs)
-audiowrite("F2_sound.wav", F2, fs)
-audiowrite("combined_sound.wav", F3, fs)
+%audiowrite("F1_sound.wav", F1, fs)
+%audiowrite("F2_sound.wav", F2, fs)
+%audiowrite("combined_sound.wav", F3, fs)
+
+%Vectors for plot
+F2P = [1100 linspace(1100,800,7)];
+F1P = repmat(700,1,8);
+F3P = 2*F1P - F2P;
+
+plot(F2P, LineWidth=2); hold on;
+plot(F1P, LineWidth=2);
+plot(F3P, '--', LineWidth=2);
+xlabel("Time (sec)")
+ylabel("Frequency (Hz)")
+legend('F2','F1', '2 x F1 - F2')
+
+plot_darkmode
+
+ax = gca;
+ax.XTickLabel = [0 1 2 3 4 5 6 7];
+ax.YLim = [200 1200]; hold off
+
+
